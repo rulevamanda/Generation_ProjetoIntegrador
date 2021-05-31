d27268df91ea05601b3448ad0e028da916709c91
Antlucas <antonio.lucas278@outlook.com>
2021-05-31 11:48:03 -0300

    Arruma erro no PostagemController

--- RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/models/Tema.java
+++ RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/models/Tema.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
 
 @Entity
 @Table(name = "tb_tema")
@@ -14,6 +17,17 @@ public class Tema {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	@NotNull
+	@Size(min = 3, max = 100)
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 	public long getId() {
 		return id;
