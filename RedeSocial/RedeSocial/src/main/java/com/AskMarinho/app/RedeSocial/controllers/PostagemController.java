f86d0563053bf1138b874823fec0ce07a5ed67fd
Bueno Vital <buenovj@hotmail.com>
2021-05-31 12:16:51 -0300

    Cria rotas para buscar todas as postagens, postagens pelo nome e por id

--- RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/controllers/PostagemController.java
+++ RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/controllers/PostagemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +28,19 @@ public class PostagemController {
 		return ResponseEntity.status(200).body(listaDePostagem);
 	}
 	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> tituloPostagem (@PathVariable String titulo) {
+		return ResponseEntity.status(200).body(repository.findAllByTituloContainingIgnoreCase(titulo));
+
+	}
+	
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Postagem> idPostagem (@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	
+	}
+	
+
+
+
 }
