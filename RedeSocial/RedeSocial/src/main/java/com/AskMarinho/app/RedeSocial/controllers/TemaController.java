256fb36f91b09cb3c7ad704ee280227eae4220ac
Matheus <matheus251170@outlook.com>
2021-06-04 11:21:28 -0300

    comitando arquivos

--- RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/controllers/TemaController.java
+++ RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/controllers/TemaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
