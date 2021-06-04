10a2c9bf642a84e9267f72225927e65231081cd7
Matheus <matheus251170@outlook.com>
2021-06-04 14:31:43 -0300

    adiciona relações ManyToMany em Tema e Postagem

--- RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/models/Postagem.java
+++ RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/models/Postagem.java
@@ -1,6 +1,8 @@
 package com.AskMarinho.app.RedeSocial.models;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -10,6 +12,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -38,6 +42,10 @@ public class Postagem {
 
 	private String url_imagem;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+
 	@OneToMany(mappedBy = "postagem", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties({"postagem", "idComentario", "usuarioComentario"})
 	private List<Comentario> comentarios;
@@ -47,46 +55,48 @@ public class Postagem {
 	@JsonIgnoreProperties({"postagens", "idUsuario", "nomeUsuario", "nomeUsuario", "nascimento", "comentarios"})
 	private Usuario usuarioPostagem;
 
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date data = new java.sql.Date(System.currentTimeMillis());
-	
-	
-	//getters and setters
+	@ManyToMany
+	@JoinTable(
+			name = "relacaoTP",
+			joinColumns = @JoinColumn(name = "fk_postagem"),
+			inverseJoinColumns = @JoinColumn(name = "fk_tema"))
+	//@JsonIgnoreProperties({"postagens","idTema"})
+	private Set<Tema> temasRelacionados = new HashSet<>();
 
 	public long getId_postagem() {
 		return id_postagem;
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public String getUrl_imagem() {
-		return url_imagem;
-	}
-
-	public Date getData() {
-		return data;
 	}
 
 	public void setId_postagem(long id_postagem) {
 		this.id_postagem = id_postagem;
 	}
 
+	public String getTitulo() {
+		return titulo;
+	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
 	}
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
+	public String getUrl_imagem() {
+		return url_imagem;
+	}
+
 	public void setUrl_imagem(String url_imagem) {
 		this.url_imagem = url_imagem;
+	}
+
+	public Date getData() {
+		return data;
 	}
 
 	public void setData(Date data) {
@@ -108,7 +118,14 @@ public class Postagem {
 	public void setUsuarioPostagem(Usuario usuarioPostagem) {
 		this.usuarioPostagem = usuarioPostagem;
 	}
-	
-	
 
+	public Set<Tema> getTemasRelacionados() {
+		return temasRelacionados;
+	}
+
+	public void setTemasRelacionados(Set<Tema> temasRelacionados) {
+		this.temasRelacionados = temasRelacionados;
+	}
+	
+	
 }
--- RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/models/Tema.java
+++ RedeSocial/RedeSocial/src/main/java/com/AskMarinho/app/RedeSocial/models/Tema.java
@@ -1,5 +1,8 @@
 package com.AskMarinho.app.RedeSocial.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,8 +25,9 @@ public class Tema {
 	@Size(min = 3, max = 100)
 	private String nome;
 
-	@ManyToMany
-	private Postagem TemaPostagem;
+	@ManyToMany(mappedBy = "temasRelacionados")
+	//@JasonIgnoreProperties({"temasRelacionados"})
+	private List<Postagem> postagens = new ArrayList<>(); 
 	
 	//getters and setters
 	public String getNome() {
