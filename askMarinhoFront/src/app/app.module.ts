3b15e8055190944b9b86c525621b1328764572d3
Antlucas <antonio.lucas278@outlook.com>
2021-07-01 11:29:38 -0300

    Cria service e consumo de API no login e cadastro

--- askMarinhoFront/src/app/app.module.ts
+++ askMarinhoFront/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +24,9 @@ import { EditPerfilComponent } from './edit-perfil/edit-perfil.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
